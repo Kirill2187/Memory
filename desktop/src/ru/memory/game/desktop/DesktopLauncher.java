42627e02298f17dfe0f01305826ecd5070f88d6c
Kirill Pupkov <pupkovkirill@gmail.com>
2020-05-06 16:42:52 +0500

    Initial commit

---
+++ desktop/src/ru/memory/game/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package ru.memory.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.memory.game.MemoryGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.resizable = false;
+		config.width = 500;
+		config.height = 400;
+
+		new LwjglApplication(new MemoryGame(), config);
+	}
+}
